a3b9f79c890259729cb5368f843d4d255de2775d
zenith110 <thequake@live.com>
2022-04-18 18:48:22 -0400

    Replaced generic variables

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform{
+    required_providers{
+        linode = {
+            source = "linode/linode"
+            version = "1.16.0"
+        }
+        cloudflare = {
+        source = "cloudflare/cloudflare"
+        version = "~> 3.0"
+    }
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+        vercel = {
+            source = "vercel/vercel"
+            version = "~> 0.1"
+        }
+    }
+}
+ backend "s3" {
+   bucket = "bucket"
+   key            = "state/terraform.tfstate"
+   region         = "region"
+   encrypt        = true
+}
+output ip_address{
+    value = linode_instance.pubsub-qa.ip_address
+}
+output vercel_deployment_url{
+    value = vercel_deployment.deployment_info.url
+}
+provider "linode" {
+    token = var.linode_api_token
+}
+provider "aws"{
+    region = var.aws_region
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+}
+provider "cloudflare"{
+    email = var.cloudflare_email
+    api_key = var.cloudflare_api_key
+}
+provider "vercel"{
+    api_token = var.vercel_api_token
+}
