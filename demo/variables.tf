89d32406fa8aad576702ec7bcb148d715f64b869
zenith110 <thequake@live.com>
2022-04-19 12:54:38 -0400

    Modified dns records to use variables and default values

--- demo/variables.tf
+++ demo/variables.tf
@@ -4,3 +4,11 @@ variable "cloudflare_zone_id" {
 variable "cloudflare_domain_name" {
     description = "Which domain will the sub-domains live under"
 }
+variable "cloudflare_dns_name" {
+    description = "Sub domain name"
+    default = "test"
+}
+variable "cloudflare_dns_ip" {
+    description = "Sub domain ip address"
+    default = "127.0.0.1"
+}
