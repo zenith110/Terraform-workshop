0d5c83dcd1361d815b6cb36f3c4c803827045478
zenith110 <thequake@live.com>
2022-04-19 20:57:15 -0400

    Added newest variables

--- demo/cloudflare.tf
+++ demo/cloudflare.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "cloud_record"{
     zone_id = var.cloudflare_zone_id
-    name  = var.cloudflare_dns_name
+    name  = var.cloudflare_domain_name
     value = var.cloudflare_dns_ip
     type = "A"
     proxied = true
--- demo/variables.tf
+++ demo/variables.tf
@@ -3,12 +3,15 @@ variable "cloudflare_zone_id" {
 }
 variable "cloudflare_domain_name" {
     description = "Which domain will the sub-domains live under"
+    default = "test.pubsub-api.dev"
 }
-variable "cloudflare_dns_name" {
-    description = "Sub domain name"
-    default = "test"
-}
+
 variable "cloudflare_dns_ip" {
     description = "Sub domain ip address"
-    default = "127.0.0.1"
+}
+variable "cloudflare_api_key"{
+    description = "api key for cloudflare"
+}
+variable "cloudflare_email"{
+    description = "cloudflare email"
 }
